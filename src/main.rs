1c0b74bc5e28915911212d6483b3a4c6fbd0cd82
warlockja <warlockja@gmail.com>
2024-03-18 23:26:34 +0300

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,45 @@
-use std::{cmp::Ordering, io};
-use rand::Rng;
+use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    let x: f64 = -5.0 / 3.0;
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // let x = x + 1;
 
-    // printing secret number
-    println!("Your secret number is {secret_number}");
-
-    loop {
-        println!("Please input your guess: ");
-    
-        let mut guess = String::new();
-        
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        
-        // crash on invalid input approach
-        // let guess: u32 = guess.trim().parse().expect("Please type a number");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {println!("Please type a number");continue}
-        };
-    
-        println!("You guessed: {guess}");
-    
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {println!("You win!"); break}
-        }
+    {
+        let x: f64 = x * 2.0;
+        println!("the value of x in the inner scope is {x}");
     }
 
+    println!("the value of x is {x}");
+
+    // tuple type
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("{x} {y} {z}");
+
+    // array in Rust has fixed length
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    // array short-hand
+    let a = [3; 5];
+
+    for n in 0..a.len() {
+        let str = a[n];
+        println!("{str}")
+    }
+
+    let mut index = String::new();
+    println!("Enter arr index");
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index.trim().parse().expect("Entered index is not a number");
+
+    // if index is a number not in 0..4 this line will throw an error
+    let element = arr[index];
+
+    println!("The value of the element is {element}")
 }
